09445e955e1f5b2b2410011bc20e1263ef416f48
yyy <501461784@qq.com>
2023-04-07 17:44:14 +0800

    update

--- update.sh
+++ update.sh
@@ -8,6 +8,6 @@ echo "上传到main分支"
 git branch -M main
 #echo "和远程仓库连接"
 #git remote add origin https://github.com/yys5014/Brush-Modules
-echo "上传远程仓库
+echo "上传远程仓库"
 git push -u origin main
 echo "上传完成！"
