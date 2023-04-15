e381f2a8c73609042a231c72afe7215972130694
yyy <501461784@qq.com>
2023-04-15 14:48:29 +0800

    update

--- code/Mi12_temperature_wall/refresh.sh
+++
@@ -1,6 +0,0 @@
-#更新信息
-Mod_information=/data/adb/modules/Mi12_temperature_wall/module.prop
-Refresh=`cat /sys/class/thermal/thermal_zone46/trip_point_3_temp`
-thousand=1000
-Refreshs=`expr $Refresh / $thousand`
-sed -i "/^description=/c description=仅适用于小米12，系统MIUI13+/14+。恢复CPU温度墙95℃，修改GPU温度墙(默认不修改，需要修改请在模块的customize.sh中更改)。配置目录：/data/media/0/Android/Mi12_temperature_wall。当前GPU墙温度[ $Refreshs℃ ]" "$Mod_information"
--- code/barrier_free_litiaotiao/uninstall.sh
+++
@@ -1,3 +0,0 @@
-#!/system/bin/sh
-rm -rf /data/adb/modules/barrier_free_litiaotiao
-rm -rf /date/dalvik-cache/* | rm -rf /date/system/package_cache/*
